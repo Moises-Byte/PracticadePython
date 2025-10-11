d4d0f441bd5447d906ab2c0889bc3e6d6861f700
Daryl Campos <dcamposd2@miumg.edu.gt>
2025-10-11 10:40:54 -0600

    Ejercicio1

---
+++ Ejerciciospy.py
@@ -0,0 +1,12 @@
+def ejercicio_if1():
+    num = float(input("Ingrese un número: "))
+    if num > 0:
+        print("El número es positivo.")
+    elif num < 0:
+        print("El número es negativo.")
+    else:
+        print("El número es cero.")
+
+#Esta parte es para ejecutar los codigos lo busque porque normalmenteno me dejo
+if __name__ == "__main__": 
+    ejercicio_if1()  
